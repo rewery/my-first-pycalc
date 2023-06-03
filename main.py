ecdb680e28e17c1b312e9e3374c4216533e793c1
rewery <rewerwer137@gmail.com>
2023-06-03 12:09:47 +0300

    simple refactoring

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ a = input('Введите матемотическое выражение:\n')
 b = a.split(' ')
 if b[1] == '+':
     print(a, '=', int(b[0]) + int(b[2]))
-if b[1] == '-':
+elif b[1] == '-':
     print(a, '=', int(b[0]) - int(b[2]))
-if b[1] == '*':
+elif b[1] == '*':
     print(a, '=', int(b[0]) * int(b[2]))
-if b[1] == '/':
+elif b[1] == '/':
     try:
         print(a, '=', int(b[0]) / int(b[2]))
     except ZeroDivisionError:
