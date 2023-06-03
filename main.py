27949d82ba7b975ce0b04eba431f6a82b27b5137
rewery <rewerwer137@gmail.com>
2023-06-03 12:06:22 +0300

    added input processing for other operations with two numbers

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ a = input('Введите матемотическое выражение:\n')
 b = a.split(' ')
 if b[1] == '+':
     print(a, '=', int(b[0]) + int(b[2]))
+if b[1] == '-':
+    print(a, '=', int(b[0]) - int(b[2]))
+if b[1] == '*':
+    print(a, '=', int(b[0]) * int(b[2]))
+if b[1] == '/':
+    try:
+        print(a, '=', int(b[0]) / int(b[2]))
+    except ZeroDivisionError:
+        print('На ноль делить нельзя')    
