d8634d647f7a24a94617a216e4350dc4690b1a79
rewery <rewerwer137@gmail.com>
2023-06-10 10:30:35 +0300

    Moved the calculation of a mathematical expression into a separate function.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+def compute(arg1: int, arg2: int, operation: str):
+    result = 0
+    if operation == '+':
+        result = arg1 + arg2
+    elif operation == '-':
+        result = arg1 - arg2
+    elif operation == '*':
+        result = arg1 * arg2
+    elif operation == '/':
+        try:
+            result = arg1 + arg2
+        except ZeroDivisionError:
+            print('На ноль делить нельзя')    
+    return result
+
 a = input('Введите матемотическое выражение:\n')
 b = a.split(' ')
-if b[1] == '+':
-    print(a, '=', int(b[0]) + int(b[2]))
-elif b[1] == '-':
-    print(a, '=', int(b[0]) - int(b[2]))
-elif b[1] == '*':
-    print(a, '=', int(b[0]) * int(b[2]))
-elif b[1] == '/':
-    try:
-        print(a, '=', int(b[0]) / int(b[2]))
-    except ZeroDivisionError:
-        print('На ноль делить нельзя')    
+print(compute(int(b[0]), int(b[2]), b[1]))
+    
+        
